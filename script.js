1f76db2aaf4e1be7b029068bee00a18d6aa91011
juliaman02 <juliana_02_1@hotmail.com>
2023-09-16 00:08:15 -0300

    Agregamos script

---
+++ script.js
@@ -0,0 +1,9 @@
+function ingresoEmail() {
+  alert("Muchas gracias por suscribirse!!");
+}
+
+function comentarios() {
+  alert(
+    "Muchas gracias por su comentario y de ser necesario nos comunicaremos a la brevedad"
+  );
+}
